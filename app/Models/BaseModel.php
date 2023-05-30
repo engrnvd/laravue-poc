2a3208c727697b4816e0457d1da23ad71a9e1285
Naveed Hassan <08es34@gmail.com>
2023-05-30 11:52:13 +0500

    * updated base model

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -2,20 +2,25 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * App\Models\BaseModel
  *
+ * @property int $id
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder|SitemapInvite newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|SitemapInvite newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|SitemapInvite query()
  * @mixin \Eloquent
- * @property int $id
- * @property-read string encrypted_id
+ * @property-read mixed $encrypted_id
  */
 class BaseModel extends Model
 {
+    protected $guarded = ["id", "created_at", "updated_at"];
+
     public function getEncryptedIdAttribute()
     {
         return encrypt($this->id);
