7ef9bb978cab79a6fecc41b048559d173fd843a2
Naveed Hassan <08es34@gmail.com>
2023-11-02 12:36:18 +0500

    * fixed api base route

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version(), 'Author' => \App\Models\User::first()->name];
+    return config('app.name');
 });
 
 Route::any('socket-io', function () {
